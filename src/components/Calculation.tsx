849ccff3d278f3a18f9f8130d2af864760026d7c
Benjamin Wright <Egonor@users.noreply.github.com>
2023-06-19 23:33:08 -0400

    - Fix Ratio Type/Props - Implemnt Event handlers for changing ratio values - Implement add ratio button - Add mutiply symbol between ratios

--- src/components/Calculation.tsx
+++ src/components/Calculation.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import Ratio, { RatioProps } from "./Ratio";
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Button, IconButton, Stack } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CircleIcon from "@mui/icons-material/Circle";
+import CloseIcon from "@mui/icons-material/Close";
+import RatioComponent, { Ratio } from "./Ratio";
 
 type CalculationProps = {};
 
 const Calculation = (props: CalculationProps) => {
-  const newRatio: RatioProps = {
-    key: 1,
+  const initialRatio: Ratio = {
+    id: 0,
     unitName: "Test Unit",
     numerator: 12,
     numeratorUnits: "meter",
@@ -14,27 +17,81 @@ const Calculation = (props: CalculationProps) => {
     denominatorUnits: "second",
   };
 
-  const [ratios, setRatios] = useState<Array<RatioProps>>([newRatio]);
+  const [ratios, setRatios] = useState<Array<Ratio>>([initialRatio]);
 
+  // Effects
   useEffect(() => {}, []);
 
+  const handleAddRatio = () => {
+    setRatios([
+      ...ratios,
+      // TODO: Get initial ratio values from Unit Picker
+      {
+        id: ratios?.length ?? 1,
+        unitName: "New Ratio",
+        numerator: 1,
+        numeratorUnits: "meter",
+        denominator: 1,
+        denominatorUnits: "second",
+      } as Ratio,
+    ]);
+  };
+
+  // Event Handlers
+  const handleChange = (newRatio: Ratio) => {
+    setRatios(
+      ratios.map((oldRatio: Ratio) => {
+        if (newRatio.id === oldRatio.id) {
+          return newRatio;
+        }
+        return oldRatio;
+      })
+    );
+  };
+
+  const calculate = (numerator: number, denominator: number): void => {};
+
   return (
-      <Stack direction={"row"} spacing={2} sx={{ flexWrap: "wrap" }}>
-        {ratios.map((ratioProps: RatioProps) => {
-          return <Ratio {...ratioProps} />;
-        })}
-        <Button
-          onClick={() => null}
-          sx={{
-            color: "primary",
-            height: "50px",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          +
-        </Button>
-      </Stack>
+    // TODO: Styling
+    //   - Fix wrap spacing (rows overlap)
+    //   - First Ratio has no left margin against paper
+    //   - Add symbol gets pushed to next row
+    //   - Multiply symbol spacing might be too much
+    <Stack
+      direction={"row"}
+      spacing={2}
+      sx={{ flexWrap: "wrap", alignItems: "center" }}
+    >
+      {ratios.map((ratio: Ratio, index: number) => {
+        return (
+          <>
+            <RatioComponent
+              key={ratio.id}
+              ratio={ratio}
+              handleChange={handleChange}
+            />
+            {/* Multiply Symbol */}
+            {ratios?.length > 1 && index !== ratios.length - 1 && (
+              // <CircleIcon sx={{ color: "secondary", scale: "0.3" }} />
+              <CloseIcon sx={{ color: "secondary", scale: "0.7" }} />
+            )}
+          </>
+        );
+      })}
+      <IconButton
+        onClick={handleAddRatio}
+        sx={{
+          display: "flex",
+          color: "secondary",
+          //height: "50px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <AddIcon sx={{ color: "secondary" }} />
+      </IconButton>
+      <></>
+    </Stack>
   );
 };
 
--- src/components/Ratio.tsx
+++ src/components/Ratio.tsx
@@ -1,35 +1,88 @@
 import { StyledComponent } from "@emotion/styled";
-import { Box, Divider, Stack } from "@mui/material";
+import { Box, Divider, InputAdornment, Stack, TextField } from "@mui/material";
 import * as React from "react";
 
-export type RatioProps = {
-  key: number,
+export type Ratio = {
+  id: number;
   unitName: string;
   numerator: number;
   numeratorUnits: string;
   denominator: number;
   denominatorUnits: string;
+};
+
+interface IRatioProps {
+  ratio: Ratio;
+  handleChange: (ratio: Ratio) => void;
 }
 
-const Ratio = (props: RatioProps) => {
+const RatioComponent = (props: IRatioProps) => {
+  const handleNumeratorChange = (event: any) => {
+    console.log(typeof event);
 
+    if (props.handleChange)
+      props.handleChange({
+        ...props.ratio,
+        numerator: event?.target.value ?? 1,
+        denominator: 1,
+      } as Ratio);
+  };
+
+  const handleDenominatorChange = (event: any) => {
+    console.log(typeof event);
+
+    if (props.handleChange)
+      props.handleChange({
+        ...props.ratio,
+        numerator: 1,
+        denominator: event?.target.value ?? 1,
+      } as Ratio);
+  };
 
   return (
     <div className="ratio">
       <Box>
-        <Stack
-          divider={<Divider flexItem sx={{color: 'green'}}/>}
-        >
-          <div className="stackItem">
-            <p className="numerator">{props.numerator} {props.numeratorUnits}{props.numerator === 1 ? "" : "s"}</p>
-          </div>
-          <div className="stackItem">
-            <p className="denominator">{props.denominator} {props.denominatorUnits}</p>
-          </div>
+        <Stack divider={<Divider flexItem sx={{ marginY: "20px" }} />}>
+          <TextField
+            className="numerator"
+            variant="outlined"
+            //type="text"
+            value={props.ratio.numerator}
+            onChange={handleNumeratorChange}
+            // TODO: Number validation (this isn't working)
+            inputProps={{
+              inputMode: "numeric",
+              pattern: "[0-9]*",
+            }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {props.ratio.numerator > 1
+                    ? props.ratio.numeratorUnits + "s"
+                    : props.ratio.numeratorUnits}
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            className="denominator"
+            variant="outlined"
+            value={props.ratio.denominator}
+            onChange={handleDenominatorChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {props.ratio.denominator > 1
+                    ? props.ratio.denominatorUnits + "s"
+                    : props.ratio.denominatorUnits}
+                </InputAdornment>
+              ),
+            }}
+          />
         </Stack>
       </Box>
     </div>
   );
 };
 
-export default Ratio;
+export default RatioComponent;
